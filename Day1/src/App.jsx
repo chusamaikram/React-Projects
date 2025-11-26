6a291e0321a7628cb20d32b6313a62e833b9aebd
chusamaikram <usamachaudhary0345@gmail.com>
2025-11-26 18:16:54 +0500

    worked on basic attributes of motion like initial, animate and transition. and also used framer motion hook useScroll to display a progress line while scroll

--- Day1/src/App.jsx
+++ Day1/src/App.jsx
@@ -1,11 +1,19 @@
 
 import './App.css'
+import BoxAnimations from './components/BoxAnimations'
+import ClickHover from './components/ClickHover'
+import ScrollAnimation from './components/ScrollAnimation'
+import TextAnimation from './components/TextAnimation'
 
 function App() {
 
   return (
     <>
-     <h1 className='font-bold text-8xl text-center'>hello react</h1>
+      <h1 className='font-bold text-8xl text-center'>Framer Motion </h1>
+      <TextAnimation />
+      <BoxAnimations />
+      <ClickHover />
+      <ScrollAnimation />
     </>
   )
 }
